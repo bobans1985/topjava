7a9df4f35bddf8223e81b2c5f8121a43e18fa209
bobans@ <bobans@inbox.ru>
2017-02-27 16:42:16 +0300

    lesson 11  finish

--- hr.bat
+++ hr.bat
@@ -1,2 +1,3 @@
-call mvn -B -s settings.xml -DskipTests=true clean package
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_111
+call d:\mvn\bin\mvn -B -s settings.xml -DskipTests=true clean package
 call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
